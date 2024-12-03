63542d4eaf3e86537f4bde00cae0be248d9dc86a
IlyasAtabaev731 <pulpasaur731@gmail.com>
2024-12-03 23:42:41 +0300

    third

--- migrations/1_init.up.sql
+++ migrations/1_init.up.sql
@@ -1,28 +1,28 @@
 CREATE TABLE orders
 (
-    order_uid          VARCHAR(50)              NOT NULL PRIMARY KEY,
-    track_number       VARCHAR(50)              NOT NULL,
-    entry              VARCHAR(10)              NOT NULL,
-    locale             VARCHAR(5)               NOT NULL,
+    order_uid          VARCHAR(50)               PRIMARY KEY,
+    track_number       VARCHAR(50)              ,
+    entry              VARCHAR(10)              ,
+    locale             VARCHAR(5)               ,
     internal_signature VARCHAR(100),
-    customer_id        VARCHAR(50)              NOT NULL,
-    delivery_service   VARCHAR(20)              NOT NULL,
-    shardkey           VARCHAR(10)              NOT NULL,
-    sm_id              INTEGER                  NOT NULL,
-    date_created       TIMESTAMP WITH TIME ZONE NOT NULL,
-    oof_shard          VARCHAR(10)              NOT NULL
+    customer_id        VARCHAR(50)              ,
+    delivery_service   VARCHAR(20)              ,
+    shardkey           VARCHAR(10)              ,
+    sm_id              INTEGER                  ,
+    date_created       TIMESTAMP WITH TIME ZONE ,
+    oof_shard          VARCHAR(10)              
 );
 
 CREATE TABLE deliveries
 (
-    order_uid VARCHAR(50)  NOT NULL,
-    name      VARCHAR(100) NOT NULL,
-    phone     VARCHAR(20)  NOT NULL,
-    zip       VARCHAR(20)  NOT NULL,
-    city      VARCHAR(100) NOT NULL,
-    address   VARCHAR(200) NOT NULL,
-    region    VARCHAR(100) NOT NULL,
-    email     VARCHAR(100) NOT NULL,
+    order_uid VARCHAR(50)  ,
+    name      VARCHAR(100) ,
+    phone     VARCHAR(20)  ,
+    zip       VARCHAR(20)  ,
+    city      VARCHAR(100) ,
+    address   VARCHAR(200) ,
+    region    VARCHAR(100) ,
+    email     VARCHAR(100) ,
     PRIMARY KEY (order_uid),
     CONSTRAINT fk_orders_delivery
         FOREIGN KEY (order_uid)
@@ -32,17 +32,17 @@ CREATE TABLE deliveries
 
 CREATE TABLE payments
 (
-    order_uid      VARCHAR(50)              NOT NULL,
-    transaction_id VARCHAR(50)              NOT NULL,
+    order_uid      VARCHAR(50)              ,
+    transaction_id VARCHAR(50)              ,
     request_id     VARCHAR(50),
-    currency       VARCHAR(10)              NOT NULL,
-    provider       VARCHAR(50)              NOT NULL,
-    amount         INTEGER                  NOT NULL,
-    payment_dt     TIMESTAMP WITH TIME ZONE NOT NULL,
-    bank           VARCHAR(50)              NOT NULL,
-    delivery_cost  INTEGER                  NOT NULL,
-    goods_total    INTEGER                  NOT NULL,
-    custom_fee     INTEGER                  NOT NULL,
+    currency       VARCHAR(10)              ,
+    provider       VARCHAR(50)              ,
+    amount         INTEGER                  ,
+    payment_dt     TIMESTAMP WITH TIME ZONE ,
+    bank           VARCHAR(50)              ,
+    delivery_cost  INTEGER                  ,
+    goods_total    INTEGER                  ,
+    custom_fee     INTEGER                  ,
     PRIMARY KEY (order_uid),
     CONSTRAINT fk_orders_payment
         FOREIGN KEY (order_uid)
@@ -53,18 +53,18 @@ CREATE TABLE payments
 CREATE TABLE items
 (
     item_id      SERIAL PRIMARY KEY,
-    order_uid    VARCHAR(50)  NOT NULL,
-    chrt_id      INTEGER      NOT NULL,
-    track_number VARCHAR(50)  NOT NULL,
-    price        INTEGER      NOT NULL,
-    rid          VARCHAR(50)  NOT NULL,
-    name         VARCHAR(100) NOT NULL,
-    sale         INTEGER      NOT NULL,
-    size         VARCHAR(10)  NOT NULL,
-    total_price  INTEGER      NOT NULL,
-    nm_id        INTEGER      NOT NULL,
-    brand        VARCHAR(100) NOT NULL,
-    status       INTEGER      NOT NULL,
+    order_uid    VARCHAR(50)  ,
+    chrt_id      INTEGER      ,
+    track_number VARCHAR(50)  ,
+    price        INTEGER      ,
+    rid          VARCHAR(50)  ,
+    name         VARCHAR(100) ,
+    sale         INTEGER      ,
+    size         VARCHAR(10)  ,
+    total_price  INTEGER      ,
+    nm_id        INTEGER      ,
+    brand        VARCHAR(100) ,
+    status       INTEGER      ,
     CONSTRAINT fk_orders_items
         FOREIGN KEY (order_uid)
             REFERENCES orders (order_uid)
